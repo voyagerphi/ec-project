3610c66bb3f28e2927a050b5b5823b4b99aa56d6
voyagerphi <voyager.phi@outlook.com>
2019-01-08 18:06:04 +0000

    Change of colors

--- refresh_map.m
+++ refresh_map.m
@@ -4,7 +4,7 @@ function refresh_map(mMap, mDrawing, OBS_ID)
                 if mMap(i,j) ~= OBS_ID
                     
                     % Color Scale Interpolation
-                    cScale = [1 1 1; 0.7 1 0.7; 0 1 1; 1 1 0.5; 0.5 1 0.5 ];                    
+                    cScale = [1 1 1; 1 1 0; 0.8 0.8 0; 0 0.7 0; 0 0.4 0];                    
                     xScale = [0 0.000001 0.05 0.5 1];                    
                     mColor = interp1(xScale, cScale, mMap(i,j));                    
                     mDrawing.mapPos{i,j}.FaceColor = mColor;                                                              
